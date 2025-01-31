1cc7765e2f737468a714df5f671d324a41665ac0
Nagacharan <nagacharan4286@gmail.com>
2025-01-31 19:41:41 +0530

    updated openwebui image

--- Terraform/sg.tf
+++ Terraform/sg.tf
@@ -11,9 +11,16 @@ resource "aws_security_group" "deepseek_sg" {
   }
 
   ingress {
-    from_port   = 5000
-    to_port     = 5000
+    from_port   = 9793
+    to_port     = 9793
     protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Restrict it if using in prod env
+  }
+
+   ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "http"
     cidr_blocks = ["0.0.0.0/0"] # Restrict it if using in prod env
   }
 
