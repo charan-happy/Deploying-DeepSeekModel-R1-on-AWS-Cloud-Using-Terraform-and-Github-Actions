d77301694cce5d4a5ebaeff21ccdd95ecc2f073c
Nagacharan <nagacharan4286@gmail.com>
2025-01-31 13:15:27 +0530

    updated configuration files of terraform

--- Terraform/outputs.tf
+++ Terraform/outputs.tf
@@ -1,11 +1,11 @@
 output "instance_public_ip" {
-  value = aws_instance.deepseekmodel.public_ip
+  value = try(aws_instance.deepseekmodel.public_ip, "No public IP available")
 }
 
 output "alb_dns_name" {
-  value = var.create_alb ? aws_lb.deepseek_alb[0].dns_name : "ALB not created"
+  value = try(var.create_alb ? aws_lb.deepseek_alb[0].dns_name : "ALB not created", "ALB not created")
 }
 
 output "s3_bucket_name" {
-  value = var.create_s3_bucket ? aws_s3_bucket.deepseek_bucket[0].id : "Bucket not created"
+  value = try(var.create_s3_bucket ? aws_s3_bucket.deepseek_bucket[0].id : "Bucket not created", "Bucket not created")
 }
